92607effcb0d729cd030d873dc03fe60e2e124d8
antoniovizarraga <145675344+antoniovizarraga@users.noreply.github.com>
2023-11-17 14:41:17 +0100

    Ejercicio 1 medio comentado

--- src/ejercicio01/Principal.java
+++ src/ejercicio01/Principal.java
@@ -4,8 +4,12 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
+		/* Variable que será el número introducido por el usuario.
+		 * Se usará para imprimir el: "Eco..." n veces que esté
+		 * en esta variable. */
 		int numUser = 0;
 		
+		// Creamos el Scanner.
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Escribe un número entero: ");
