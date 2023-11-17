1a1b34d0072d0d7843ebf76f51cb8f844f4b7986
antoniovizarraga <145675344+antoniovizarraga@users.noreply.github.com>
2023-11-17 14:39:54 +0100

    Ejercicio 1 terminado

---
+++ src/ejercicio01/Echo.java
@@ -0,0 +1,9 @@
+package ejercicio01;
+
+public class Echo {
+	public static void eco(int echo) {
+		for(int i = 0; i < echo; i++) {
+			System.out.println("Eco...");
+		}
+	} 
+}
--- src/ejercicio01/Principal.java
+++ src/ejercicio01/Principal.java
@@ -1,10 +1,19 @@
 package ejercicio01;
+import java.util.Scanner;
 
 public class Principal {
 
 	public static void main(String[] args) {
+		int numUser = 0;
 		
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Escribe un número entero: ");
+		numUser = sc.nextInt();
+		
+		Echo.eco(numUser);
+		
+		sc.close();
 	}
 
 }
