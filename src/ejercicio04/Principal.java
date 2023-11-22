b90003e9921f90e53c77e161767b183337b8b21a
antoniovizarraga <145675344+antoniovizarraga@users.noreply.github.com>
2023-11-22 08:58:19 +0100

    Ejercicio 4 cambiado para que pida el input del usuario

--- src/ejercicio04/Principal.java
+++ src/ejercicio04/Principal.java
@@ -1,5 +1,8 @@
 package ejercicio04;
 
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class Principal {
 
 	/* Vamos a crear una aplicación que diga si una letra (Char) introducida a nivel de código
@@ -7,11 +10,22 @@ public class Principal {
 	 * usuario), es una vocal o no. Usaremos una clase que devuelva un booleano para ello. */
 	public static void main(String[] args) {
 		// Creamos el char con el que comprobaremos si la letra introducida es una vocal.
-		char character = 'a';
+		String character = "";
 		
 		// Creamos el boolean que usaremos para saber si char es una vocal o no.
 		final boolean IS_VOCAL;
 		
+		Scanner sc = new Scanner(System.in);
+		
+		do {
+			try {
+				System.out.print("Introduce una letra: ");
+				character = sc.next();
+			} catch(InputMismatchException e) {
+				System.err.println("Error. Se ha introducido un valor no válido. Vuelve a intentarlo.");
+				sc.next();
+			}
+		} while(character.equals(""));
 		/* Asignamos a la variable de referencia la comprobación de si el character es una vocal o no 
 		 * gracias a la función creada en la clase Vocal. */
 		IS_VOCAL = Vocal.vocalCheck(character);
@@ -21,6 +35,8 @@ public class Principal {
 			System.out.println("Es una vocal.");
 		else
 			System.out.println("No es una vocal.");
+		
+		sc.close();
 		
 	}
 
--- src/ejercicio04/Vocal.java
+++ src/ejercicio04/Vocal.java
@@ -3,41 +3,14 @@ package ejercicio04;
 public class Vocal {
 	/* Creamos una función boolean que compruebe que un valor dado en el código del método
 	 * main es una vocal o no. Para ello usaremos un char como valor. */
-	public static boolean vocalCheck(char userInput) {
+	public static boolean vocalCheck(String userInput) {
 		// Crea la variable de referencia que luego devolveremos al método main para saber si es vocal o no.
 		final boolean IS_VOCAL;
 		
 		/* Haz un switch case en el que dependiendo de si tiene vocales, le asignas el valor:
 		 * "true" al boolean. Si no, por defecto le asignas el valor: "false". */
 		switch(userInput) {
-		case 'a':
-			IS_VOCAL = true;
-			break;
-		case 'A':
-			IS_VOCAL = true;
-			break;
-		case 'e':
-			IS_VOCAL = true;
-			break;
-		case 'E':
-			IS_VOCAL = true;
-			break;
-		case 'i':
-			IS_VOCAL = true;
-			break;
-		case 'I':
-			IS_VOCAL = true;
-			break;
-		case 'o':
-			IS_VOCAL = true;
-			break;
-		case 'O':
-			IS_VOCAL = true;
-			break;
-		case 'u':
-			IS_VOCAL = true;
-			break;
-		case 'U':
+		case "a", "A", "e", "E", "i", "I", "o", "O", "u", "U":
 			IS_VOCAL = true;
 			break;
 		default:
